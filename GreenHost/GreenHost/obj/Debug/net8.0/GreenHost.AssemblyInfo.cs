faf446fadd2643590bdc7c68b80107f55eb51da4
Farid447 <faridrn-ab108@code.ede.az>
2025-01-23 19:01:09 +0400

    son

--- GreenHost/GreenHost/obj/Debug/net8.0/GreenHost.AssemblyInfo.cs
+++ GreenHost/GreenHost/obj/Debug/net8.0/GreenHost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a66fc5949b7e9ddcaf708324e670b7cd0a587e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
